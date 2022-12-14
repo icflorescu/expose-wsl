474c01c6fe6ba21fe2f7a7badc108a7c1cdf62f1
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2022-12-14 15:18:05 +0200

    Dtore binary in package folder

--- run.js
+++ run.js
@@ -2,26 +2,23 @@
 import figlet from 'figlet';
 import { execSync } from 'node:child_process';
 import { existsSync, mkdirSync } from 'node:fs';
-import { tmpdir } from 'node:os';
 import { dirname } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import delay from './utils/delay.js';
 import download from './utils/download.js';
 
-const folder = `${tmpdir()}/expose-wsl`;
+const folder = dirname(fileURLToPath(import.meta.url));
 
 console.log(figlet.textSync('expose-wsl'), '\n');
 
-console.log(dirname(fileURLToPath(import.meta.url)));
-
-if (!existsSync(folder)) {
+if (!existsSync(`${folder}/WSLHostPatcher.exe`)) {
   process.stdout.write('WSLHostPatcher not found, downloading... ');
   mkdirSync(folder);
   await download(
     'https://github.com/CzBiX/WSLHostPatcher/releases/download/v0.1.1/WSLHostPatcher.zip',
     `${folder}/WSLHostPatcher.zip`
   );
-  process.stdout.write('extracting files... ');
+  process.stdout.write('extracting... ');
   execSync(`unzip ${folder}/WSLHostPatcher.zip -d ${folder}`);
   execSync(`rm ${folder}/WSLHostPatcher.zip`);
   execSync(`chmod +x ${folder}/WSLHostPatcher.exe`);
