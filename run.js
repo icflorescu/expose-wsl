3655c1994f75c715571ebec01aa6418177bb4a7b
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2023-08-04 16:09:03 +0300

    Add better error handling

--- run.js
+++ run.js
@@ -17,34 +17,43 @@ if (existsSync(`${dir}/WSLHostPatcher.zip`)) unlinkSync(`${dir}/WSLHostPatcher.z
 if (existsSync(`${dir}/WSLHostPatch.dll`)) unlinkSync(`${dir}/WSLHostPatch.dll`);
 if (existsSync(`${dir}/WSLHostPatcher.exe`)) unlinkSync(`${dir}/WSLHostPatcher.exe`);
 
-process.stdout.write('⏳ Downloading WSLHostPatcher... ');
-await download(
-  'https://github.com/CzBiX/WSLHostPatcher/releases/latest/download/WSLHostPatcher.zip',
-  `${dir}/WSLHostPatcher.zip`
-);
-process.stdout.write('extracting... ');
-await extract(`${dir}/WSLHostPatcher.zip`, { dir });
+let success = false;
+try {
+  process.stdout.write('⏳ Downloading WSLHostPatcher... ');
+  await download(
+    'https://github.com/CzBiX/WSLHostPatcher/releases/latest/download/WSLHostPatcher.zip',
+    `${dir}/WSLHostPatcher.zip`
+  );
+  process.stdout.write('extracting... ');
+  await extract(`${dir}/WSLHostPatcher.zip`, { dir });
 
-// Remove the zip file
-unlinkSync(`${dir}/WSLHostPatcher.zip`);
+  // Remove the zip file
+  unlinkSync(`${dir}/WSLHostPatcher.zip`);
 
-// Make sure the patcher is executable
-chmodSync(`${dir}/WSLHostPatcher.exe`, 0o755);
+  // Make sure the patcher is executable
+  chmodSync(`${dir}/WSLHostPatcher.exe`, 0o755);
 
-// Wait a bit to make sure the patcher is ready, then run it
-await delay(100);
-process.stdout.write('running... ');
-execSync(`${dir}/WSLHostPatcher.exe`);
+  // Wait a bit to make sure the patcher is ready, then run it
+  await delay(100);
+  process.stdout.write('running... ');
+  execSync(`${dir}/WSLHostPatcher.exe`);
 
-// Remove the patcher files
-unlinkSync(`${dir}/WSLHostPatcher.exe`);
-unlinkSync(`${dir}/WSLHostPatch.dll`);
+  console.log('done ✔️');
+  process.stdout.write('🎉 WSL should be accessible at: ');
+  execSync(
+    "powershell.exe 'Get-NetIPAddress -AddressFamily IPv4 -InterfaceIndex $(Get-NetConnectionProfile | Select-Object -ExpandProperty InterfaceIndex) | Select-Object -ExpandProperty IPAddress'",
+    { stdio: 'inherit' }
+  );
+  console.log('💡 Make sure to restart your server application(s) before trying to access them!');
+  success = true;
+} catch (err) {
+  console.log('failed to run ❌');
+  console.log('🔥 Please report this issue at https://github.com/icflorescu/expose-wsl/issues');
+  console.log(err.stderr ? err.stderr.toString() : err);
+} finally {
+  // Remove the patcher files
+  unlinkSync(`${dir}/WSLHostPatcher.exe`);
+  unlinkSync(`${dir}/WSLHostPatch.dll`);
+}
 
-console.log('done ✔️');
-console.log('💡 Make sure to restart your server application(s) before trying to access them!\n');
-process.stdout.write('🎉 WSL should be accessible at: ');
-execSync(
-  "powershell.exe 'Get-NetIPAddress -AddressFamily IPv4 -InterfaceIndex $(Get-NetConnectionProfile | Select-Object -ExpandProperty InterfaceIndex) | Select-Object -ExpandProperty IPAddress'",
-  { stdio: 'inherit' }
-);
-process.exit(0);
+process.exit(success ? 0 : 1);
